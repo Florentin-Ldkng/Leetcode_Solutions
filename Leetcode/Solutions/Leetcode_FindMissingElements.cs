137af72bdaae7d0a03e7c1b086060af5205aaa75
Florentin <florentin.luedeking@t-online.de>
2026-08-10 09:52:59 +0200

    Update `.gitignore` and documentation links
    Exclude `.vs/` directory from source control to prevent committing IDE-specific files.
    Refactor Leetcode solution documentation links to use `` tags for improved readability and IDE integration.

--- Leetcode/Solutions/Leetcode_FindMissingElements.cs
+++ Leetcode/Solutions/Leetcode_FindMissingElements.cs
@@ -9,8 +9,8 @@ namespace Leetcode.Solutions
 
         /// <summary>
         /// 3731. Find Missing Elements
-        /// Problem: https://leetcode.com/problems/find-missing-elements
-        /// Submission: https://leetcode.com/submissions/detail/2098424452/
+        /// Problem: <see href="https://leetcode.com/problems/find-missing-elements"/>
+        /// Submission: <see href="https://leetcode.com/submissions/detail/2098424452/"/>
         /// </summary>
         public IList<int> FindMissingElements(int[] nums)
         {
