2384047ff3b4551cdaf072fc1840ee099122f12a
Florentin <florentin.luedeking@t-online.de>
2026-08-11 12:18:09 +0200

    Add Leetcode problem 28: Find the Index of the First Occurrence in a String
    Implements the solution for Leetcode's 'Find the Index of the First Occurrence in a String' problem.
    
    A dedicated solution file (`Leetcode_FindTheIndexOfTheFirstOccurenceInAString.cs`) has been added for this problem, including its official problem and submission links. The `Solution.cs` file is updated to contain the `StrStr` implementation, replacing the previously active `RemoveElement` method. The test case in `Program.cs` is adjusted to verify the new `StrStr` solution.

--- Leetcode/Solutions/Leetcode_FindTheIndexOfTheFirstOccurenceInAString.cs
+++ Leetcode/Solutions/Leetcode_FindTheIndexOfTheFirstOccurenceInAString.cs
@@ -1,46 +1,48 @@
-﻿using System.Collections;
-using System.Xml.Linq;
-
-namespace Leetcode
+﻿namespace Leetcode.Solutions
 {
-    public class Solution
+    internal class Leetcode_FindTheIndexOfTheFirstOccurenceInAString
     {
+        /// <summary>
+        /// 28. Find the Index of the First Occurrence in a String
+        /// Problem: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string
+        /// Submission: https://leetcode.com/submissions/detail/2102670434/
+        /// </summary>
         public int StrStr(string haystack, string needle)
         {
-            Dictionary<char,int> needleDict = new Dictionary<char, int>();
-            
+            Dictionary<char, int> needleDict = new Dictionary<char, int>();
+
             bool found = false;
-            
+
             for (int i = 0; i < needle.Length - 1; i++)
             {
                 needleDict[needle[i]] = needle.Length - i - 1;
             }
-            
+
             int j = needle.Length - 1;
-            
+
             while (j < haystack.Length)
             {
                 int counter = 0;
-            
+
                 for (int i = needle.Length - 1; i >= 0; i--)
-                {            
+                {
                     if (needle[i] == haystack[j - counter])
                     {
                         counter++;
                     }
                     else
-                    {            
+                    {
                         break;
                     }
                 }
-            
+
                 if (counter == needle.Length)
                 {
                     found = true;
                     break;
                 }
-            
-            
+
+
                 if (needleDict.ContainsKey(haystack[j]))
                 {
                     j += needleDict[haystack[j]];
@@ -51,10 +53,10 @@ namespace Leetcode
                     j += needle.Length;
                 }
             }
-            
+
             if (found)
                 return j - (needle.Length - 1);
-            
+
             return -1;
         }
     }
